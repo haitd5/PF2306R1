f1615f4bdf37ed75f6d1c043c43a08dafb4d98d6
haitd5 <haitran1606@gmail.com>
2023-07-05 21:17:03 +0900

    Day 04 - Variables, Data Type and Operators

---
+++ day04-variable, data type and operators/demo/intro/assets/js/main.js
@@ -0,0 +1,19 @@
+// function showMessage(){
+//     alert('Xin chào!');
+// }
+
+// let yourName = prompt("Hãy nhập tên của bạn");
+
+// document.write("<br>Xin chào " + yourName);
+
+// let width = prompt("Width");
+// let height = prompt("Height");
+// let area = width * height;
+// document.write("<br>Area = " + area);
+
+let a = Number(prompt("nhap a"));
+let b = Number(prompt("nhap b"));
+
+if (a % b == 0) {
+  document.write("a chia het cho b");
+} else document.write("a chia het cho b");
